ec2525c8195f6a8f0d719e12a1ec78eb10ce1690
Mark <s.markmas@gmail.com>
2018-07-22 18:15:49 +0530

    Routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import ReviewPage from "./Components/ReviewPage";
+import Form from "./Components/Form";
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//add routes here
+
+ReactDOM.render(
+    <Router>
+      <div>
+       <Route exact path="/" component={App}/>
+       <Route path="/form" component={Form}/>   
+       </div>
+    </Router>,
+    document.getElementById('root')
+  );
 registerServiceWorker();
